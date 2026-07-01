24afaa08b062672b73a00e5ca6a21f84dbaed774
BarMal <barnabymalaj@gmail.com>
2026-07-01 12:36:25 +0100

    Extract command runner settings input schema

---
+++ src/test/scala/com/serenity/CommandRunnerSettingsInputItemsSpec.scala
@@ -0,0 +1,44 @@
+package com.serenity
+
+import com.serenity.command.{CommandIntent, CommandRunnerSettingsInputItems, CommandSurfaceItem}
+import com.serenity.config.{AppConfig, SpellCheckConfig}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class CommandRunnerSettingsInputItemsSpec extends AnyFlatSpec with Matchers:
+
+  private def inputById(items: List[CommandSurfaceItem.InputItem], id: String): CommandSurfaceItem.InputItem =
+    items.find(_.id == id).getOrElse(fail(s"missing input item $id"))
+
+  "CommandRunnerSettingsInputItems" should "build config-backed settings input rows independently of runner state" in {
+    val config = AppConfig.default
+      .withUiElementGap(3)
+      .withCommandRunnerVisibleRows(Some(9))
+      .withSpellCheck(SpellCheckConfig(enabled = true, languages = List("EN", "fr"), additionalWords = List("Cats")))
+
+    val items = CommandRunnerSettingsInputItems.build(config)
+
+    inputById(items, "ui-element-gap").currentValue shouldBe "3"
+    inputById(items, "ui-element-gap").parse("4") shouldBe Some(CommandIntent.SetUiElementGap(4))
+    inputById(items, "ui-element-gap").parse("9") shouldBe None
+    inputById(items, "command-runner-visible-rows").currentValue shouldBe "9"
+    inputById(items, "command-runner-visible-rows").parse("auto") shouldBe
+      Some(CommandIntent.SetCommandRunnerVisibleRows(None))
+    inputById(items, "spellcheck-languages").currentValue shouldBe "en,fr"
+    inputById(items, "spellcheck-languages").parse("fr,en") shouldBe
+      Some(CommandIntent.SetSpellCheckLanguages(List("fr", "en")))
+    inputById(items, "spellcheck-words").currentValue shouldBe "cats"
+  }
+
+  it should "build preset and binding parsers as reusable schema rows" in {
+    val items = CommandRunnerSettingsInputItems.build(AppConfig.default)
+
+    inputById(items, "ui-preset-create").parse("Focus") shouldBe Some(CommandIntent.SaveUiPreset("Focus"))
+    inputById(items, "ui-preset-rename").parse("Focus -> Review") shouldBe
+      Some(CommandIntent.RenameUiPreset("Focus", "Review"))
+    inputById(items, "ui-preset-rename").parse("Focus") shouldBe None
+    inputById(items, "keymap-global-command_palette").parse("ctrl+k") shouldBe
+      Some(CommandIntent.SetGlobalHotkey(com.serenity.config.HotkeyAction.ToggleCommandRunner, "ctrl+k"))
+    inputById(items, "keymap-global-command_palette").parse("reset") shouldBe
+      Some(CommandIntent.ResetGlobalHotkey(com.serenity.config.HotkeyAction.ToggleCommandRunner))
+  }
