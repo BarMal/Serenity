ba4d966070ac3ab2f69e395be1d5d0d944a4f22a
BarMal <barnabymalaj@gmail.com>
2026-07-02 13:22:43 +0100

    Show close workflow actions for empty drafts

--- src/main/scala/com/serenity/ui/layout/LayoutEngine.scala
+++ src/main/scala/com/serenity/ui/layout/LayoutEngine.scala
@@ -456,9 +456,10 @@ object LayoutEngine:
             if workflow.statusMessage.nonEmpty then 8 else 7
           case Modal.Find(_, results, _) =>
             if results.nonEmpty then 5 else 4
-          case Modal.CloseWorkflow(_) => 4
-          case Modal.Custom(_, _)     => 4
-          case _                      => 3
+          case Modal.CloseWorkflow(_) =>
+            SurfaceFrameLayout.frameHeightForItemRows(itemRows = 2, hasHeader = true, hasFooter = false)
+          case Modal.Custom(_, _) => 4
+          case _                  => 3
       case SurfaceContent.Terminal(_, _) | SurfaceContent.Outline(_, _) | SurfaceContent.Diagnostics(_) |
           SurfaceContent.MarkdownPreview(_, _) =>
         math.min(8, math.max(4, maxHeight - 1))
