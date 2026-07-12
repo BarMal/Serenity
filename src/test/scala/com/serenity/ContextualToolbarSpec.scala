d8acf3816236ae632563c700ff6b5f1b011610f2
BarMal <barnabymalaj@gmail.com>
2026-07-12 15:11:04 +0100

    Default formatting toolbar to icons

--- src/test/scala/com/serenity/ContextualToolbarSpec.scala
+++ src/test/scala/com/serenity/ContextualToolbarSpec.scala
@@ -25,7 +25,7 @@ class ContextualToolbarSpec extends AnyFlatSpec with Matchers with StateManagerT
       opened.activeCursorPosition,
       SurfacePlacement.BelowCursor
     )
-    toolbarStateFrom(opened).displayMode shouldBe ToolbarDisplayMode.IconAndText
+    toolbarStateFrom(opened).displayMode shouldBe ToolbarDisplayMode.IconOnly
 
     stateManager.applyEvent(ToggleCommandRunner).unsafeRunSync()
 
