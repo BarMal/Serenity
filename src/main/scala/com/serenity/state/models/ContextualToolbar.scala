f44f0981cc48c549ad640768aa88a9391cfc4d12
BarMal <BarMal@users.noreply.github.com>
2026-07-18 21:35:15 +0000

    Render toolbar controls as icon glyphs

--- src/main/scala/com/serenity/state/models/ContextualToolbar.scala
+++ src/main/scala/com/serenity/state/models/ContextualToolbar.scala
@@ -264,13 +264,13 @@ object ContextualToolbar:
       case ContextualToolbarItem.Dropdown(_, label, icon, optionItem) =>
         val text = s"$label ${optionItem.selectedOption}".trim
         mode match
-          case ToolbarDisplayMode.IconOnly    => s"${optionItem.selectedOption} ▾"
+          case ToolbarDisplayMode.IconOnly    => icon
           case ToolbarDisplayMode.TextOnly    => text
           case ToolbarDisplayMode.IconAndText => s"$icon $text"
       case ContextualToolbarItem.Input(_, label, icon, inputItem) =>
         val text = s"$label ${inputItem.currentValue}".trim
         mode match
-          case ToolbarDisplayMode.IconOnly    => inputItem.currentValue
+          case ToolbarDisplayMode.IconOnly    => icon
           case ToolbarDisplayMode.TextOnly    => text
           case ToolbarDisplayMode.IconAndText => s"$icon $text"
 
