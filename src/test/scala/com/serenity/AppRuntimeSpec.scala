5b1c000844125a7a3e9eacc481a5f43350c3c800
BarMal <BarMal@users.noreply.github.com>
2026-07-19 21:25:55 +0100

    Skip resize polls for keyboard input (#787)

--- src/test/scala/com/serenity/AppRuntimeSpec.scala
+++ src/test/scala/com/serenity/AppRuntimeSpec.scala
@@ -136,6 +136,7 @@ class AppRuntimeSpec extends AnyFlatSpec with Matchers:
   it should "avoid refreshing the focused translator for ordinary editor events" in {
     val program = for
       refreshes     <- Ref.of[IO, Int](0)
+      resizeChecks  <- Ref.of[IO, Int](0)
       cursorVisible <- Ref.of[IO, Boolean](true)
       breathIndex   <- Ref.of[IO, Int](0)
       router = new InputRouter[IO, Event]:
@@ -154,15 +155,62 @@ class AppRuntimeSpec extends AnyFlatSpec with Matchers:
         def readText: IO[Option[String]]      = IO.pure(None)
         def writeText(text: String): IO[Unit] = IO.unit
       _ <- AppRuntime
-        .inputEventPhase(stateManager, router, clipboard, IO.unit, cursorVisible, breathIndex, IO.unit)(
+        .inputEventPhase(
+          stateManager,
+          router,
+          clipboard,
+          resizeChecks.update(_ + 1),
+          cursorVisible,
+          breathIndex,
+          IO.unit
+        )(
           Stream.emits(List(InsertChar('a'), DeleteBackward, MoveLeft, InsertChar('b')))
         )
         .compile
         .drain
-      count <- refreshes.get
+      refreshCount <- refreshes.get
+      resizeCount  <- resizeChecks.get
+    yield (refreshCount, resizeCount)
+
+    program.unsafeRunSync() shouldBe (0, 0)
+  }
+
+  it should "check for a resize before applying pointer input" in {
+    val program = for
+      resizeChecks  <- Ref.of[IO, Int](0)
+      cursorVisible <- Ref.of[IO, Boolean](true)
+      breathIndex   <- Ref.of[IO, Int](0)
+      router = new InputRouter[IO, Event]:
+        private val initialTranslator = new TextEntryTranslator(AppConfig.default)
+
+        def eventStream(infoStream: Stream[IO, KeyStrokeInfo]): Stream[IO, Event] = Stream.empty
+        def setActiveTranslator(translator: Translator[Event]): IO[Unit]          = IO.unit
+        def getActiveTranslator: IO[Translator[Event]]                            = IO.pure(initialTranslator)
+      stateManager = new com.serenity.state.manager.StateReader
+        with com.serenity.state.manager.StateUpdater
+        with com.serenity.state.manager.EventApplier:
+        def getCurrentState: IO[AppState]                       = IO.pure(AppState.initial)
+        def updateState(update: AppState => AppState): IO[Unit] = IO.unit
+        def applyEvent(event: Event): IO[Unit]                  = IO.unit
+      clipboard = new SystemClipboard[IO]:
+        def readText: IO[Option[String]]      = IO.pure(None)
+        def writeText(text: String): IO[Unit] = IO.unit
+      _ <- AppRuntime
+        .inputEventPhase(
+          stateManager,
+          router,
+          clipboard,
+          resizeChecks.update(_ + 1),
+          cursorVisible,
+          breathIndex,
+          IO.unit
+        )(Stream.emit(MousePress(0, 0)))
+        .compile
+        .drain
+      count <- resizeChecks.get
     yield count
 
-    program.unsafeRunSync() shouldBe 0
+    program.unsafeRunSync() shouldBe 1
   }
 
   it should "refresh the focused translator after a modal request" in {
